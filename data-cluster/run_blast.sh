c6aa18ed2ce922ad8a75f0149efd7a59f2ebd579
Peter van Heusden <pvh@sanbi.ac.za>
2016-03-08 11:59:11 +0200

    * Fix bug in run_blast.sh

--- data-cluster/run_blast.sh
+++ data-cluster/run_blast.sh
@@ -7,5 +7,5 @@ QUERY=/cip0/research/projects/cluster_demo/sample.fa
 BLAST_OPTS='6 qseqid sseqid evalue bitscore sgi sacc staxids sscinames scomnames stitle'
 BLASTDB=/cip0/db/ncbi/nt/default
 export BLASTDB
-blastn -db /cip0/db/ncbi/nt/default/nt -max_target_seqs 1 -outfmt $BLAST_OPTS \
+blastn -db /cip0/db/ncbi/nt/default/nt -max_target_seqs 1 -outfmt "$BLAST_OPTS" \
   -evalue "1e-30" -query $QUERY -out blast_results.txt
